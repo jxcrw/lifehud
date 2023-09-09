5195034b1819ba869ed9ca0366a6ba20504ffe2e
Jak Crow <jak@jxcrw.io>
2023-09-09 02:06:55 -0700

    ~trk: Improve path handling in lang tracker

--- _cfg/config.py
+++ _cfg/config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Settings"""
 
+import os
 from pathlib import Path
 import sys
 
@@ -9,5 +10,6 @@ DIR_HOME = Path.home()
 DIR_ROOT = Path(sys.path[1])
 DIR_DATA = DIR_ROOT / '_arc/data'
 DIR_SYNC = DIR_HOME / 'Dropbox/lifehud'
+DIR_SCOOP = Path(os.getenv('SCOOP'))
 
 DIR_DATA = DIR_SYNC  # TODO
--- tracker/lang.py
+++ tracker/lang.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 """Get study data from Anki"""
 
+import os
 from pathlib import Path
 import shutil
 import sqlite3
 
-from _cfg.config import DIR_DATA, DIR_SYNC
+from _cfg.config import DIR_DATA, DIR_SCOOP, DIR_SYNC
 
 
 # Setup
-FILE_IN = Path(r'C:\scoop\persist\anki\data\Jak\collection.anki2')
-FILE_WORK = Path(DIR_SYNC / 'collection.anki2')
-shutil.copy(FILE_IN, FILE_WORK)
+FILE = DIR_SYNC / 'lang.tsv'
+FILE_ORIG = DIR_SCOOP / 'persist/anki/data/Jak/collection.anki2'
+FILE_WORK = Path(DIR_DATA / 'collection.anki2')
+shutil.copy(FILE_ORIG, FILE_WORK)
 
 
 # Extract data
@@ -29,6 +31,7 @@ ORDER BY date
 """
 
 raw = c.execute(query).fetchall()
+con.close()
 
 
 # Parse/format data
@@ -41,6 +44,9 @@ for entry in reversed(raw):
 
 
 # Write data
-path = DIR_DATA / 'lang.txt'
-with open(path, 'w+', newline='\n', encoding='utf-8') as f:
+with open(FILE, 'w+', newline='\n', encoding='utf-8') as f:
     f.write('\n'.join(['\t'.join(entry) for entry in data]))
+
+
+# Clean up
+os.remove(FILE_WORK)
