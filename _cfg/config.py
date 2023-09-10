0fa6cc3f98593d6d03b025377646962c6d938f56
Jak Crow <jak@jxcrw.io>
2023-09-10 06:12:09 -0700

    +hud2: Use pandas dataframes for data 😎

--- _cfg/config.py
+++ _cfg/config.py
@@ -6,10 +6,10 @@ from pathlib import Path
 import sys
 
 
+# Pathing
 DIR_HOME = Path.home()
 DIR_ROOT = Path(sys.path[1])
 DIR_DATA = DIR_ROOT / '_arc/data'
 DIR_SYNC = DIR_HOME / 'Dropbox/lifehud'
 DIR_SCOOP = Path(os.getenv('SCOOP'))
-
 DIR_DATA = DIR_SYNC  # TODO
--- lifehud2.py
+++ lifehud2.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python3
 """LifeHUD2"""
+
+import os
+from pathlib import Path
+
+from pandas import DataFrame
+import pandas as pd
+
+from _cfg.config import DIR_SYNC
+
+
+def load_dataframes() -> dict[str, DataFrame]:
+    """Load up dataframes of life data."""
+    dfs = {}
+    for root, dirs, files in os.walk(DIR_SYNC):
+        files = [Path(root, file) for file in files if '0' in file]
+        for file in files:
+            df = pd.read_csv(file, sep='\t')
+            dfs[file.stem] = df
+    return dfs
+
+
+if __name__ == '__main__':
+    dfs = load_dataframes()
