fd53379df3bb9566ba8c52595ffb7db08075c3f0
Jak Crow <jak@jxcrw.io>
2023-09-14 09:02:51 -0700

    +hud2: Start work on proper cli

--- lifehud2.py
+++ lifehud2.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python3
 """LifeHUD2"""
 
-from datetime import date
+from datetime import date, timedelta
+
+import click
 
 from _cfg.config import SMART_TODAY
 from project import Project
 
 
-if __name__ == '__main__':
-    # ┌─────────────────────────────────────────────────────────────────────────────
-    # │ Setup
-    # └─────────────────────────────────────────────────────────────────────────────
-    # Determine smart today
-    mind = Project('0', 'hours', 6.9, 7.9, 25, True, 'Sun Mon Tue Wed Thu Fri Sat', SMART_TODAY)
+# ┌─────────────────────────────────────────────────────────────────────────────
+# │ Init
+# └─────────────────────────────────────────────────────────────────────────────
+PROJECTS = [
+    Project('0', 'hours', 6.9, 7.9, 25, True, 'Sun Mon Tue Wed Thu Fri Sat', SMART_TODAY),
+    Project('0', 'hours', 6.9, 7.9, 25, True, 'Sun Mon Tue Wed Thu Fri Sat', SMART_TODAY),
+]
 
-    # Initialize other projects
-    PROJECTS = [
-        mind,
-        # Project('0', 'hours', 6.9, 7.9, 25, False, 'Sun Mon Tue Wed Thu Fri Sat', date.today()),
-        # Project('0', 'hours', 6.9, 7.9, 25, False, 'Sun Mon Tue Wed Thu Fri Sat', date.today()),
-    ]
-    PROJECTS = {p.name: p for p in PROJECTS}
+
+# ┌─────────────────────────────────────────────────────────────────────────────
+# │ CLI
+# └─────────────────────────────────────────────────────────────────────────────
+@click.group()
+def cli():
+    pass
+
+
+if __name__ == '__main__':
+    # command()
+    print()
+    cli()
