6d5221a3ed29e4f184c7b77d0634e868ccb79bad
Jak Crow <jak@jxcrw.io>
2023-09-19 06:17:06 -0700

    ~cfg: Tweak project definitions towards reality

--- cfg/projects.py
+++ cfg/projects.py
@@ -7,10 +7,10 @@ from lib.project import Project
 
 PROJECTS = [
     Project('mind', 'hours', 6.9, 7.9, 25, False, WEEKMASK_ALL, SMART_TODAY),
-    Project('body', 'hours', 0.1, 0.1, 20, True, WEEKMASK_SIX, SMART_TODAY),
+    Project('body', 'hours', 0.9, 1.5, 20, True, WEEKMASK_SIX, SMART_TODAY),
     Project('pool', 'hours', 0.9, 1.9, 00, True, WEEKMASK_ALL, SMART_TODAY),
-    Project('lang', 'hours', 0.1, 0.1, 00, False, WEEKMASK_ALL, SMART_TODAY),
-    Project('work', 'hours', 1.0, 3.0, 00, True, WEEKMASK_ALL, SMART_TODAY),
-    Project('meet', 'hours', 0.1, 0.1, 00, True, WEEKMASK_ANY, SMART_TODAY),
+    Project('lang', 'hours', 0.2, 0.4, 00, False, WEEKMASK_ALL, SMART_TODAY),
+    Project('work', 'hours', 2.0, 4.0, 00, True, WEEKMASK_ALL, SMART_TODAY),
+    Project('meet', 'hours', 0.1, 1.0, 00, True, WEEKMASK_ANY, SMART_TODAY),
 ]
 PROJECTS = {p.name: p for p in PROJECTS}
